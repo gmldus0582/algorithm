cce43c1d337fe22e1cfbe22374ed7d02765f6d79
HeeyeonPark <gmldus0582@naver.com>
2022-10-10 16:37:23 +0900

    dijkstra를 이용함

--- road.py
+++ road.py
@@ -1,3 +1,8 @@
+#출발지랑 목적지가 정해져있음으로 dijkstra 이용하기 
+#그리고 거리를 재야함으로 
+#https://swexpertacademy.com/main/solvingProblem/solvingProblem.do 
+#SWEA 보급로 문제 
+
 from heapq import heappop, heappush
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -12,17 +17,21 @@ def dijkstra(num,graph,x,y):
     distance[0][0] = 0
     dx =[1,-1,0,0]
     dy = [0,0,1,-1]
-    
 
     while q:
         x,y,cost = heappop(q)
+        #distance 배열 값 보다 cost가 크면 굳이 돌 필요 없음
         if distance[x][y] < cost:
             continue
+        #상하좌우 방향 다 돌아보기 
         for i in range(4):
             nx= x+dx[i]
             ny= y+dy[i]
+
+            #보급로 나가는 경우는 pass
             if 0 >nx or nx >=num or ny<0 or ny >= num:
                 continue
+            #다음 노드의 길이 = 현재 노드의 길이 + 다음 노드 값 
             cost = distance[x][y]+ graph[nx][ny]
             if cost <distance[nx][ny]:
                 distance[nx][ny] =cost
